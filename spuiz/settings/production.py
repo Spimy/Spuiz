246f0c15953910a36b4957a5412e794e944e925a
Spimy <officialspyme@gmail.com>
2020-09-07 20:01:39 +0400

    Update database engine

--- spuiz/settings/production.py
+++ spuiz/settings/production.py
@@ -11,7 +11,7 @@ INSTALLED_APPS += [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DATABASE_NAME'),
         'HOST': os.getenv('DATABASE_HOST'),
         'USER': os.getenv('DATABASE_USER'),
