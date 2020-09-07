522724b12057b27189c0d2c0565f9df3515f263c
Spimy <officialspyme@gmail.com>
2020-09-07 22:00:12 +0400

    Add case insensitive field mixin

---
+++ spuiz/mixins.py
@@ -0,0 +1,15 @@
+class CaseInsensitiveFieldMixin:
+    '''
+    Field mixin that uses case-insensitive lookup alternatives if they exist.
+    '''
+
+    LOOKUP_CONVERSIONS = {
+        'exact': 'iexact',
+        'contains': 'icontains',
+        'startswith': 'istartswith',
+        'endswith': 'iendswith',
+        'regex': 'iregex',
+    }
+
+    def get_lookup(self, lookup_name):
+        converted = self.LOOKUP_CONVERSIONS.get(lookup_name, lookup_name)
