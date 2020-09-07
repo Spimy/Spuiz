77442e92be5c510dc0db5de2dc702e5090a0aaba
Spimy <officialspyme@gmail.com>
2020-09-07 20:26:01 +0400

    Set setting as environment variable

--- manage.py
+++ manage.py
@@ -6,8 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'spuiz.settings.development')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('SETTING'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
