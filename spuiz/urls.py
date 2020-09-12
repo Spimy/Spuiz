85635eec3dfcfe245c88c653dff8eade76655fe2
Spimy <officialspyme@gmail.com>
2020-09-12 23:07:59 +0400

    Include quiz app url in main project

--- spuiz/urls.py
+++ spuiz/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.accounts.urls'))
+    path('', include('apps.accounts.urls')),
+    path('quiz/', include('apps.quiz.urls')),
 ]
 
 if settings.DEBUG:
