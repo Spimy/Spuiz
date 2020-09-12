26e34ccfc510e3ee0d5cdc81f1b5521440338eb0
Spimy <officialspyme@gmail.com>
2020-09-12 23:07:15 +0400

    Add forms for all models

---
+++ apps/quiz/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from .models import Quiz, Question, Answer
+
+
+class QuizForm(forms.ModelForm):
+
+    author = forms.ModelChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.HiddenInput
+    )
+
+    mcq = forms.BooleanField(label='Multiple Choice', require=False)
+
+    class Meta:
+        model = Quiz
+        exclude = ['slug', 'upvotes', 'downvotes']
+
+
+class QuestionForm(forms.ModelForm):
+
+    class Meta:
+        model = Question
+        exclude = ['']
+
+
+class AnswerForm(forms.ModelForm):
+
+    answer = forms.CharField(label='')
+
+    class Meta:
+        model = Answer
+        exclude = ['question']
+
+
+QuestionFormSet = forms.inlineformset_factory(
+    parent_model=Quiz,
+    model=Question,
+    form=QuestionForm,
+    extra=1,
+    can_delete=False
+)
+
+AnswerFormSet = forms.inlineformset_factory(
+    parent_model=Question,
+    model=Answer,
+    form=AnswerForm,
+    extra=1,
+    can_delete=False
+)
