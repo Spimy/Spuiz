1ff6324247299100e7ef22fefcefd02f82be2840
Spimy <officialspyme@gmail.com>
2020-09-12 15:28:11 +0400

    Model refactor

--- apps/quiz/models.py
+++ apps/quiz/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth import get_user_model
-from spuiz.fields import CICharField
+from spuiz import fields
 
 
 class Question(models.Model):
@@ -14,6 +14,7 @@ class Question(models.Model):
 class Answer(models.Model):
     answer = models.CharField(max_length=255)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    correct = models.BooleanField(default=True)
 
     def __str__(self):
         return self.answer
@@ -21,10 +22,20 @@ class Answer(models.Model):
 
 class Quiz(models.Model):
     slug = models.SlugField(max_length=255, unique=True, blank=True)
-    title = models.CICharField(max_length=255, unique=True)
+    title = fields.CICharField(max_length=255, unique=True)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    upvotes = models.ManyToManyField(get_user_model(), blank=True)
-    downvotes = models.ManyToManyField(get_user_model(), blank=True)
+
+    upvotes = models.ManyToManyField(
+        get_user_model(),
+        related_name='quiz_upvotes',
+        blank=True
+    )
+    downvotes = models.ManyToManyField(
+        get_user_model(),
+        related_name='quiz_downvotes',
+        blank=True
+    )
+
     thumbnail = models.ImageField(upload_to='quizzes/thumbnails', blank=True)
 
     mcq = models.BooleanField(default=True)
